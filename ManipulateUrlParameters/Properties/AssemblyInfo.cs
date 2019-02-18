7d4bab2970ca7bfb8c403f2d49a66d83920c7587
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-18 19:51:54 +0300

    Добавьте файлы проекта.

---
+++ ManipulateUrlParameters/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ManipulateUrlParameters")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ManipulateUrlParameters")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd0f35df-a486-4ec6-a475-63cc451ad346")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
