df7ee8812fa952241c725b77ac6c89835c3a7bd5
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-18 21:17:25 +0300

    1

---
+++ ManipulateUrlParameters/ManipulateUrlParameters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulateUrlParameters
+{
+    public class ManipulateUrlParameters
+    {
+        public static string AddOrChangeUrlParameter(string url, string param)
+        {
+            int index = url.IndexOf('?');
+            int index1 = url.IndexOf('=');
+            int index2 = param.IndexOf('=');
+            Console.WriteLine(index2);
+            string st = "", result = "";
+            for (int i = 0; i < index2; i++)
+            {
+                st += param[i];
+            }
+            if (url.Contains(st))
+            {
+                result = url.Remove(index + 1);
+                result = result.Insert(index + 1, param);
+            }
+            else
+            {
+                if(index1 == -1)
+                    result = url + "?" + param;
+                else
+                    result = url + "&" + param;
+            }               
+            return result;
+        }
+    }
+}
---
+++ ManipulateUrlParameters/PublicTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ManipulateUrlParameters
+{
+    [TestFixture]
+    public class PublicTest
+    {
+        [TestCase("www.example.com", "key=value", ExpectedResult = "www.example.com?key=value")]
+        [TestCase("www.example.com?key=value", "key2=value2", ExpectedResult = "www.example.com?key=value&key2=value2")]
+        [TestCase("www.example.com?key=oldValue`", "key=newValue", ExpectedResult = "www.example.com?key=newValue")]
+        [TestCase("http://example.com/", "ID=1234", ExpectedResult = "http://example.com/?ID=1234")]
+        [TestCase("http://example.com?ID=666", "ID=1234", ExpectedResult = "http://example.com?ID=1234")]
+        [TestCase("http://example.com?Name=Allan", "ID=1234", ExpectedResult = "http://example.com?Name=Allan&ID=1234")]
+        public static string AllCharactersAreUniqueTests(string url, string param) => ManipulateUrlParameters.AddOrChangeUrlParameter(url, param);
+   
+    }
+}
